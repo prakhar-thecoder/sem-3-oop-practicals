b4d86c0e3387b293bd68de0cf90057288e11fd68
Prakhar Parikh <109915897+prakhar-thecoder@users.noreply.github.com>
2024-07-31 11:45:50 +0530

    Added Practical - 1 c

---
+++ P1C.java
@@ -0,0 +1,17 @@
+public class P1C {
+    public static void main(String[] args) {
+        int a = 1;
+        char b = 'b';
+        float c = 3.14f;
+        double d = 3.14;
+        boolean e = true;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Character: " + b);
+        System.out.println("Float: " + c);
+        System.out.println("Double: " + d);
+        System.out.println("Boolean: " + e);
+
+        System.out.println("Main name is: Prakhar Parikh!");
+    }
+}
