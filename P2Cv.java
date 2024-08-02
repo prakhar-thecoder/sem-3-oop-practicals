873a6d967db535e82580016b655b0b85e82fb2f9
Prakhar Parikh <109915897+prakhar-thecoder@users.noreply.github.com>
2024-08-02 15:33:30 +0530

    Added Practical 2 - C (v)

---
+++ P2Cv.java
@@ -0,0 +1,15 @@
+public class P2Cv {
+    public static void main(String[] args) {
+        int n = 6;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < (n - i) * 2; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
+}
