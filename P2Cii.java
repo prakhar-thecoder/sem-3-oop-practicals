6785e95e5b04b6341caa2f6b41c6a04dda84bf7d
Prakhar Parikh <109915897+prakhar-thecoder@users.noreply.github.com>
2024-08-02 14:55:48 +0530

    Update P2Cii.java

--- P2Cii.java
+++ P2Cii.java
@@ -1,5 +1,13 @@
 public class P2Cii {
     public static void main(String[] args) {
+        char c = 'a';
         
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            System.out.println(c + " is a vowel");
+        } else if (c == '_' || c == '.' || c == '-') {
+            System.out.println(c + " is a special character");
+        } else {
+            System.out.println(c + " is a consonent");
+        }
     }
 }
